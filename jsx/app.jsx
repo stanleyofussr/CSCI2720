dd515c4e842acb78a70930d653e476fdb285d51a
wanruuu <1155107790@link.cuhk.edu.hk>
2020-05-07 17:10:02 +0800

    adjust frame

--- jsx/app.jsx
+++
@@ -1,7 +0,0 @@
-class App extends React.Component {
-	render() {
-		return <p>here</p>
-	}
-}
-
-ReactDOM.render(<App name="Timeline" />, document.querySelector("#app"));
---
+++ jsx/navigationBar.jsx
@@ -0,0 +1,7 @@
+/* include only navigation bar */
+class NavigationBar extends React.Component {
+	render() {
+		return <p>navigation bar</p>;
+	}
+}
+ReactDOM.render(<NavigationBar/>, document.querySelector("#app"));
