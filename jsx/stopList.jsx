1c53835f5880f90896e63a5d0d7e8759a1e00110
Sspark <ssparkluo@gmail.com>
2020-05-07 20:22:00 +0800

    create stopList and add it to index.html

---
+++ jsx/stopList.jsx
@@ -0,0 +1,9 @@
+class ShowList extends React.Component{
+    render() {
+        return(
+            <p>a</p>
+        )
+    }
+}
+
+ReactDOM.render(<ShowList />, document.getElementById('stopList'))
