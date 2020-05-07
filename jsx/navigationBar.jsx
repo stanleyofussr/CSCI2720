de6f2adf55d21fafa97822cb283d0a68772f617f
wanruuu <1155107790@link.cuhk.edu.hk>
2020-05-08 03:55:38 +0800

    log out test

--- jsx/navigationBar.jsx
+++ jsx/navigationBar.jsx
@@ -27,7 +27,7 @@ class LogOut extends React.Component {
 	leave = e => {this.setState({hover: false});}
 	render() {
 		return (
-			<span className="logIcon" onMouseEnter={(e) => this.enter(e)} onMouseLeave={(e) => this.leave(e)}>
+			<span className="logIcon" onMouseEnter={(e) => this.enter(e)} onMouseLeave={(e) => this.leave(e)} onClick={this.props.logout}>
 				<svg focusable="false" viewBox="0 0 512 512" width="28px">
 					<title>{"Log Out"}</title>
 					<path fill={this.state.hover?"#656B6F":"black"} d="M497 273L329 441c-15 15-41 4.5-41-17v-96H152c-13.3 0-24-10.7-24-24v-96c0-13.3 10.7-24 24-24h136V88c0-21.4 25.9-32 41-17l168 168c9.3 9.4 9.3 24.6 0 34zM192 436v-40c0-6.6-5.4-12-12-12H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h84c6.6 0 12-5.4 12-12V76c0-6.6-5.4-12-12-12H96c-53 0-96 43-96 96v192c0 53 43 96 96 96h84c6.6 0 12-5.4 12-12z"></path>
@@ -95,18 +95,27 @@ class LogInStatus extends React.Component {
 			this.setState({username: data.username});
 		});
 	}
+	logout = () => {
+		fetch('http://localhost:8000/logout', {
+			method: 'post'
+		})
+		.then(res => res.json())
+		.then(data => {
+			console.log(data);
+		});
+	}
 	render() {
 		if(this.state.admin == null)
 			return (
 				<span>
 					{this.state.username == null ? <SignUp/> : <a>{this.state.username}</a>}
-					{this.state.username == null ? <LogIn login={this.login}/> : <LogOut/>}
+					{this.state.username == null ? <LogIn login={this.login}/> : <LogOut logout={this.logout}/>}
 				</span>
 			);
 		return (
 			<span>
 				<a>{"Admin"}</a>
-				<LogOut/>
+				<LogOut logout={this.logout}/>
 			</span>
 		);
 	}
