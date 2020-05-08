7593ada986b4f5aabebe25cce9a3dfcff6584af2
Sspark <ssparkluo@gmail.com>
2020-05-09 00:48:02 +0800

    create UI of stop detail

--- src/stopDetail.js
+++ src/stopDetail.js
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default class StopDetail extends React.Component {
 	render() {
-		return <p>StopDetail here</p>;
+		return (
+			<div className="modal fade bd-example-modal-lg" id={"Modal" + this.props.stopName} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div className="modal-dialog modal-lg" role="document">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+							<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div className="modal-body">
+							{this.props.stopName}
+						</div>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="button" className="btn btn-primary">Save changes</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
 	}
 }
--- src/stopList.js
+++ src/stopList.js
@@ -58,7 +58,7 @@ export default class StopList extends React.Component {
         //TODO Ajax
         var index = favourite.findIndex(element => element.stopname == name);
         this.setState({
-            temp: favourite.splice(index,1)
+            temp: favourite.splice(index, 1)
         })
         //TODO notice
     }
@@ -74,11 +74,13 @@ export default class StopList extends React.Component {
             <div className="container">
                 <div className="card-columns mt-3 text-center">
                     {this.state.filteredData.map(stop =>
-                        <StopItem key = {stop._objectID} stopName={stop.stopname} longtitude={stop.longtitude} latitude={stop.latitude} commentNum={stop.comment.length} 
-                        addFavHandler = {this.addFavHandler} delFavHandler = {this.delFavHandler} inFavourite={favourite.find(element => element.stopname == stop.stopname)} />
+                        <StopItem key={stop._objectID} stopName={stop.stopname} longtitude={stop.longtitude} latitude={stop.latitude} commentNum={stop.comment.length}
+                            addFavHandler={this.addFavHandler} delFavHandler={this.delFavHandler} inFavourite={favourite.find(element => element.stopname == stop.stopname)} />
                     )}
                 </div>
-                <StopDetail />
+                {this.state.stopListData.map(stop =>
+                    <StopDetail stopName = {stop.stopname}/>
+                )}
             </div>
         )
     }
@@ -93,13 +95,13 @@ class StopItem extends React.Component {
                         <div className="col-10 text-left"><h4>{this.props.stopName}</h4></div>
                         <div className="col-2 d-flex justify-content-center align-items-center">
                             {this.props.inFavourite ? (
-                                <svg onClick = {(e) => this.props.delFavHandler(this.props.stopName,e)} className="bi bi-heart-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="#ea0000" xmlns="http://www.w3.org/2000/svg">
+                                <svg onClick={(e) => this.props.delFavHandler(this.props.stopName, e)} className="bi bi-heart-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="#ea0000" xmlns="http://www.w3.org/2000/svg">
                                     <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" clipRule="evenodd" />
                                 </svg>) : (
-                                    <svg onClick = {(e) => this.props.addFavHandler(this.props.stopName,e)} className="bi bi-heart" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <svg onClick={(e) => this.props.addFavHandler(this.props.stopName, e)} className="bi bi-heart" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fillRule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 01.176-.17C12.72-3.042 23.333 4.867 8 15z" clipRule="evenodd" />
                                     </svg>
-                                    )
+                                )
                             }
                         </div>
                     </div>
@@ -113,9 +115,10 @@ class StopItem extends React.Component {
                         <small className="text-muted"><p className="card-text">Comment number: {this.props.commentNum}</p></small>
                     </div>
                 </div>
-                <div className = "card-footer text-primary">
-                    <a className = "btn" href = {this.props.addFavHandler} >Location detail</a>
+                <div className="card-footer text-primary">
+                    <a className="btn" data-toggle="modal" data-target={"#Modal" + this.props.stopName}>Location detail</a>
                 </div>
+
             </div>
         )
     }
