57b7464ac089ab57ae6a9e5c9ac7c8653b678cea
Sspark <ssparkluo@gmail.com>
2020-05-09 22:36:42 +0800

    Add testData, style change

--- src/stopList.js
+++ src/stopList.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import $ from 'jquery';
 import StopDetail from './stopDetail.js'
+
+const testData = [
+    { stopid: "000001", stopname: "test1", longtitude: 123, latitude: 123, comment: [] },
+    { stopid: "000002", stopname: "test2", longtitude: 143, latitude: 56, comment: [] },
+    { stopid: "000003", stopname: "test3", longtitude: 23, latitude: 11, comment: [] },
+    { stopid: "000004", stopname: "test4", longtitude: 45, latitude: 9, comment: [] },
+    { stopid: "000005", stopname: "test5", longtitude: 12, latitude: 145, comment: [] },
+    { stopid: "000006", stopname: "test6", longtitude: 67, latitude: 123, comment: [] },
+]
 
 export default class StopList extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            stopListData: [],
-            filteredData: [],
+            stopListData: testData,
+            filteredData: testData,
             favourite: [],
             temp: null,
             page: "stoplist",
@@ -19,23 +27,23 @@ export default class StopList extends React.Component {
         var stopList;
         var favouriteList;
 
-
-        fetch('http://localhost:8000/stop/', {
-            method: 'get',
-            mode: 'cors',
-            credentials: 'include',
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (data.login != 0 || data.login == null) {
-                    stopList = data;
-                }
-                this.setState({
-                    stopListData: stopList,
-                    filteredData: stopList,
-                })
-            });
-
+        /*
+            fetch('http://localhost:8000/stop/', {
+                method: 'get',
+                mode: 'cors',
+                credentials: 'include',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.login != 0 || data.login == null) {
+                        stopList = data;
+                    }
+                    this.setState({
+                        stopListData: stopList,
+                        filteredData: stopList,
+                    })
+                });
+    */
         fetch('http://localhost:8000/favourite/', {
             method: 'get',
             mode: 'cors',
@@ -159,6 +167,19 @@ export default class StopList extends React.Component {
     }
 
     //TODO: FOR SORTING
+    sorting = (sortBy, type, event) => {
+        var sortedData = this.state.filteredData.sort((data1, data2) => {
+            var a = data1[sortBy];
+            var b = data2[sortBy];
+            if (type == "up")
+                return a > b;
+            else
+                return b > a;
+        })
+        this.setState({
+            filteredData: sortedData
+        })
+    }
 
     //FOR ADMIN, delete the location from database
     delItemHandler = event => {
@@ -174,16 +195,17 @@ export default class StopList extends React.Component {
     render() {
         return (
             <div className="container">
-                <div className="card-columns mt-3 text-center">
-                    {this.state.filteredData.map(stop =>
-                        <StopItem key={stop._objectID} stopName={stop.stopname} longtitude={stop.longtitude} latitude={stop.latitude} commentNum={stop.comment.length}
-                            addFavHandler={this.addFavHandler} delFavHandler={this.delFavHandler} inFavourite={this.state.favourite.find(element => element.stopname == stop.stopname)} />
-                    )}
+                <div className="d-flex justify-content-center">
+                    <div className="card-columns mt-3 text-center">
+                        {this.state.filteredData.map(stop =>
+                            <StopItem key={stop._objectID} stopName={stop.stopname} longtitude={stop.longtitude} latitude={stop.latitude} commentNum={stop.comment.length}
+                                addFavHandler={this.addFavHandler} delFavHandler={this.delFavHandler} inFavourite={this.state.favourite.find(element => element.stopname == stop.stopname)} />
+                        )}
+                    </div>
                 </div>
-                {this.state.stopListData.map(stop =>
-                    <StopDetail stopName={stop.stopname} isAdmin={true} />
-                )}
-            </div>
+                <StopDetail />
+                <button className="button btn btn-primary" onClick={(e) => this.sorting("longtitude", "up", e)}>click</button>
+            </div >
         )
     }
 }
@@ -191,45 +213,78 @@ export default class StopList extends React.Component {
 class StopItem extends React.Component {
     render() {
         return (
-            <div className="stopItem card bg-light">
-                <div className="card-header">
-                    <div className="row">
-                        <div className="col-12 "><h4>{this.props.stopName}
-                        <span className="float-right">
-                            {this.props.inFavourite ? (
-                                <svg data-toggle="popover" data-content="Some" onClick={(e) => this.props.delFavHandler(this.props.stopName, e)} className="bi bi-heart-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="#ea0000" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" clipRule="evenodd" />
-                                </svg>) : (
-                                    <svg onClick={(e) => this.props.addFavHandler(this.props.stopName, e)} className="bi bi-heart" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path fillRule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 01.176-.17C12.72-3.042 23.333 4.867 8 15z" clipRule="evenodd" />
-                                    </svg>
-                                )
-                            }
-                        </span>
-                        </h4>
-                        </div>
-                    </div>
-                </div>
-                <div className="card-body">
-                    <div className="row">
-                        <div className="col-6">
-                            <h5 className="card-title">Location longtitude:</h5>
-                            <p className="card-text">{this.props.longtitude}</p>
-                        </div>
-                        <div className="col-6">
-                            <h5 className="card-title">Location latitude:</h5>
-                            <p className="card-text">{this.props.latitude}</p>
-                        </div>
-                    </div>
-                    <div className="text-center mt-1">
-                        <small className="text-muted"><p className="card-text">Comment number: {this.props.commentNum}</p></small>
-                    </div>
-                </div>
-                <div className="card-footer text-primary">
-                    <a className="btn" data-toggle="modal" data-target={"#Modal" + this.props.stopName}>Location detail</a>
-                </div>
 
+            <div className="card mb-3">
+                <div className="row no-gutters">
+                    <div className="col-md-4 bg-light d-flex justify-content-center align-items-center">
+                        <div><h2 className="font-weight-normal text-body text-wrap">{this.props.stopName}</h2></div>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <span className="float-right">
+                                {this.props.inFavourite ? (
+                                    <svg data-toggle="popover" data-content="Some" onClick={(e) => this.props.delFavHandler(this.props.stopName, e)} className="bi bi-heart-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="#ea0000" xmlns="http://www.w3.org/2000/svg">
+                                        <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" clipRule="evenodd" />
+                                    </svg>) : (
+                                        <svg onClick={(e) => this.props.addFavHandler(this.props.stopName, e)} className="bi bi-heart" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 01.176-.17C12.72-3.042 23.333 4.867 8 15z" clipRule="evenodd" />
+                                        </svg>
+                                    )
+                                }
+                            </span>
+                            <div className="row">
+                                <div className="col-6">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                                <div className="col-6">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            /*            <div className="stopItem card bg-light">
+                            <div className="card-header">
+                                <div className="row">
+                                    <div className="col-12 "><h4>{this.props.stopName}
+                                        <span className="float-right">
+                                            {this.props.inFavourite ? (
+                                                <svg data-toggle="popover" data-content="Some" onClick={(e) => this.props.delFavHandler(this.props.stopName, e)} className="bi bi-heart-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="#ea0000" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" clipRule="evenodd" />
+                                                </svg>) : (
+                                                    <svg onClick={(e) => this.props.addFavHandler(this.props.stopName, e)} className="bi bi-heart" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                        <path fillRule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 01.176-.17C12.72-3.042 23.333 4.867 8 15z" clipRule="evenodd" />
+                                                    </svg>
+                                                )
+                                            }
+                                        </span>
+                                    </h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-6">
+                                        <h5 className="card-title">Location longtitude:</h5>
+                                        <p className="card-text">{this.props.longtitude}</p>
+                                    </div>
+                                    <div className="col-6">
+                                        <h5 className="card-title">Location latitude:</h5>
+                                        <p className="card-text">{this.props.latitude}</p>
+                                    </div>
+                                </div>
+            
+                            </div>
+                            <div className="card-footer text-primary">
+                                <a className="btn" data-toggle="modal" data-target={"#Modal" + this.props.stopName}>Location detail</a>
+                            </div>
+            
+                        </div>
+                        */
         )
     }
 }
