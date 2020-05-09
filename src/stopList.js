2aae030e6154c0543025d4a48331fcdce5d4b0c7
Sspark <ssparkluo@gmail.com>
2020-05-09 15:21:54 +0800

    add fetch func in stoplist

--- src/stopList.js
+++ src/stopList.js
@@ -2,39 +2,63 @@ import React from 'react';
 import $ from 'jquery';
 import StopDetail from './stopDetail.js'
 
-const testData = [
-    { _objectID: "1", stopname: "test1", longtitude: 123.21, latitude: 32.123, arrival: [12.32, 13.32], comment: ['test', 'test2'] },
-    { _objectID: "2", stopname: "test2", longtitude: 123.21, latitude: 32.123, arrival: [12.32, 13.32], comment: ['test', 'test2'] },
-    { _objectID: "3", stopname: "test3", longtitude: 123.21, latitude: 32.123, arrival: [12.32, 13.32], comment: ['test', 'test2'] },
-    { _objectID: "4", stopname: "test4", longtitude: 123.21, latitude: 32.123, arrival: [12.32, 13.32], comment: ['test', 'test2'] },
-    { _objectID: "5", stopname: "test5", longtitude: 123.21, latitude: 32.123, arrival: [12.32, 13.32], comment: ['test', 'test2'] },
-    { _objectID: "6", stopname: "test6", longtitude: 123.21, latitude: 32.123, arrival: [12.32, 13.32], comment: ['test', 'test2'] },
-    { _objectID: "7", stopname: "test7", longtitude: 123.21, latitude: 32.123, arrival: [12.32, 13.32], comment: ['test', 'test2'] },
-    { _objectID: "8", stopname: "test8", longtitude: 123.21, latitude: 32.123, arrival: [12.32, 13.32], comment: ['test', 'test2'] },
-    { _objectID: "9", stopname: "longlonglonglonglonglonglongTESTlonglonglong", longtitude: 123.21, latitude: 32.123, arrival: [12.32, 13.32], comment: ['test', 'test2'] },
-];
-const favourite = [
-    { _objectID: "4", stopname: "test4", longtitude: 123.21, latitude: 32.123, arrival: [12.32, 13.32], comment: ['test', 'test2'] },
-];
-
 export default class StopList extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            stopListData: testData,
-            filteredData: testData,
-            favourite: favourite,
+            stopListData: [],
+            filteredData: [],
+            favourite: [],
             temp: null,
             page: "stoplist",
-            username : this.props.username,
+            username: this.props.username,
+            admin: this.props.admin
         };
+
+        var stopList;
+        var favouriteList;
+        
+
+        fetch('http://localhost:8000/stop/', {
+            method: 'get',
+            mode: 'cors',
+            credentials: 'include',
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.login != 0 || data.login == null) {
+                    stopList = data;
+                }
+                this.setState({
+                    stopListData: stopList,
+                    filteredData: stopList,
+                })
+            });
+
+        fetch('http://localhost:8000/favourite/', {
+            method: 'get',
+            mode: 'cors',
+            credentials: 'include',
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.login != 0 || data.login == null) {
+                    favouriteList = data;
+                }
+                this.setState({
+                    favourite: favouriteList
+                })
+            });
+
+
     }
     componentWillReceiveProps(nextProps) {
         if (nextProps.page == "favourite") {
             switch (nextProps.searchType) {
                 case "stopname":
                     this.setState({
-                        filteredData: favourite.filter(item => {
+                        filteredData: this.state.favourite.filter(item => {
                             if (item.stopname.toLowerCase().includes(nextProps.search.toLowerCase()))
                                 return true;
                             return false;
@@ -43,7 +67,7 @@ export default class StopList extends React.Component {
                     break;
                 case "longitude":
                     this.setState({
-                        filteredData: favourite.filter(item => {
+                        filteredData: this.state.favourite.filter(item => {
                             if (item.stopname.toString().includes(nextProps.search))
                                 return true;
                             return false;
@@ -52,7 +76,7 @@ export default class StopList extends React.Component {
                     break;
                 case "latitude":
                     this.setState({
-                        filteredData: favourite.filter(item => {
+                        filteredData: this.state.favourite.filter(item => {
                             if (item.latitude.toString().includes(nextProps.search))
                                 return true;
                             return false;
@@ -96,26 +120,44 @@ export default class StopList extends React.Component {
 
     //FOR USER, add the location to favourite
     addFavHandler = (name, event) => {
-        //TODO Ajax
-        var index = testData.findIndex(element => element.stopname == name);
-        var stopData = testData[index];
-        this.setState({
-            temp: favourite.push(stopData)
+        fetch('http://localhost:8000/favourite/' + name, {
+            method: 'put',
+            mode: 'cors',
+            credentials: 'include',
         })
-        alert("Location has been added to your favourite.")
+            .then(res => res.json())
+            .then(data => {
+                if (data.stopAdded == 1) {
+                    var index = this.state.stopListData.findIndex(element => element.stopname == name);
+                    var stopData = this.state.stopListData[index];
+                    this.setState({
+                        temp: this.state.favourite.push(stopData)
+                    })
+                    alert("Location has been added to your favourite.")
+                }
+            });
     }
 
     //FOR USER, delete the location from favourite
     delFavHandler = (name, event) => {
-        //TODO Ajax
-        var index0 = favourite.findIndex(element => element.stopname == name);
-        var index1 = this.state.filteredData.findIndex(element => element.stopname == name)
-        
-        this.setState({
-            temp: favourite.splice(index0, 1),
-            temp: this.state.filteredData.splice(index1 ,1)
+        fetch('http://localhost:8000/favourite/' + name, {
+            method: 'DELETE',
+            mode: 'cors',
+            credentials: 'include'
         })
-        alert("Location has been removed from your favourite.")
+            .then(res => res.json)
+            .then(data => {
+                if (data.stopRemoved == 1) {
+                    var index0 = this.state.favourite.findIndex(element => element.stopname == name);
+                    var index1 = this.state.filteredData.findIndex(element => element.stopname == name)
+
+                    this.setState({
+                        temp: this.state.favourite.splice(index0, 1),
+                        temp: this.state.filteredData.splice(index1, 1)
+                    })
+                    alert("Location has been removed from your favourite.")
+                }
+            })
     }
 
     //FOR ADMIN, delete the location from database
@@ -135,7 +177,7 @@ export default class StopList extends React.Component {
                 <div className="card-columns mt-3 text-center">
                     {this.state.filteredData.map(stop =>
                         <StopItem key={stop._objectID} stopName={stop.stopname} longtitude={stop.longtitude} latitude={stop.latitude} commentNum={stop.comment.length}
-                            addFavHandler={this.addFavHandler} delFavHandler={this.delFavHandler} inFavourite={favourite.find(element => element.stopname == stop.stopname)} />
+                            addFavHandler={this.addFavHandler} delFavHandler={this.delFavHandler} inFavourite={this.state.favourite.find(element => element.stopname == stop.stopname)} />
                     )}
                 </div>
                 {this.state.stopListData.map(stop =>
